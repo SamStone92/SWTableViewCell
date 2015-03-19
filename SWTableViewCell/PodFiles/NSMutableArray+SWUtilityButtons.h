776d9820a99966ed2c89fe5a795cdb32305f7bd7
Sam Stone <samstone920@googlemail.com>
2015-03-19 13:54:44 +0000

    Fixed UIKit bug
    Fixed the UIKit bug in NSMutableArray+SWUtilityButtons.h

--- SWTableViewCell/PodFiles/NSMutableArray+SWUtilityButtons.h
+++ SWTableViewCell/PodFiles/NSMutableArray+SWUtilityButtons.h
@@ -5,7 +5,7 @@
 //  Created by Matt Bowman on 11/27/13.
 //  Copyright (c) 2013 Chris Wendel. All rights reserved.
 //
-
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
 @interface NSMutableArray (SWUtilityButtons)
